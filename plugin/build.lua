e6e37b6e88f8cb9ed037ab54e2169cc4012fad96
Rubenduburck <rubenduburck@gmail.com>
2024-02-24 21:26:15 +0000

    build file not working

--- plugin/build.lua
+++
@@ -1,9 +0,0 @@
--- build Rust plugin
-local build_command = "cargo build --release > /dev/null 2>&1"
-local result = os.execute(build_command)
-
-if result == 0 then
-  print("Build succeeded")
-else
-  print("Build failed")
-end
